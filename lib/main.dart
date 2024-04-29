535618a390f316a2dcf843b2e856ee148cef69b4
VladislavZhyliakov <80688590+VladislavZhyliakov@users.noreply.github.com>
2024-04-29 17:07:57 +0300

    Created develop branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     const MaterialApp(
-      home: Text('Hello World, it\'s Freud!'),
+      home: Text('Hello World, it\'s Freud!\nNow Starting Development!'),
     )
   );
 }
